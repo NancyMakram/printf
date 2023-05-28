5e550c049682a46cffc170c98c09f7fcf8fd641b
Sarah Ahmed <sarah.ahmed9765@gmail.com@gmail.com>
2023-05-27 23:04:48 -0700

    specifier

--- specifier.c
+++ specifier.c
@@ -12,6 +12,9 @@ int (*get_specifier(char *s))(va_list ap, params_t *params)
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
+		{"b", print_binary},
+		{"p", print_address},
+		{"R", print_rot13},
 		{NULL, NULL}
 	};
 	int i = 0;
@@ -25,4 +28,64 @@ int (*get_specifier(char *s))(va_list ap, params_t *params)
 		i++;
 	}
 	return (NULL);
+}
+
+/**
+ * get_print_func - finds the format func
+ * @s: format string
+ * @ap: argument pointer
+ * @params: parameters struct
+ *
+ * Return: number of bytes printed
+ */
+int get_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+
+/**
+ * get_flag - finds the flag func
+ * @s: dormat string
+ * @params: parameters struct
+ *
+ * Return: if flag is valid
+ */
+int get_flag(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+ * get_modifier - finds the modifier func
+ * @s: format string
+ * @params: parameters struct
+ *
+ * Return: if modifier is valid
+ */
+int get_modifier(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case 'h':
+			i = params->h_modifier = 1;
+			break;
+		case'l':
+			i = params->l_modifier = 1;
+			break;
+	}
+	return (i);
 }
